030f482ddcbfbf46c67a7c293dd404987a27f7d8
krishnan2002 <sit19cs108@sairamtap.edu.in>
2023-05-18 21:42:16 +0530

    second commit

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,7 +1,19 @@
+const fs =require('fs')
+
 const getNotes =function(){
     return "Your notes..."
 }
 const addNote = function (title,body){
+    try{
+        const data=(fs.readFileSync(notes.json)).toString()
+    }catch(e){
+        data = []
+    }
+    data.push({
+        title: title,
+        body: body
+    })
+    fs.writeFileSync('notes.json',JSON.stringify(data))
 
 }
 module.exports={
