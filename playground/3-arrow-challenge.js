4ac84382c22c80f4a71d11e5f283caae50696d88
krishnan2002 <sit19cs108@sairamtap.edu.in>
2023-05-20 09:50:44 +0530

    first commit

---
+++ playground/3-arrow-challenge.js
@@ -0,0 +1,16 @@
+const tasks ={
+    tasks:[{
+        text:'Grocery shopping',
+        completed: true
+    },{
+        text: "Clean yard",
+        completed: false
+    },{
+        text: 'film course',
+        completed: false
+    }],
+    getTasksToDo(){
+        return this.tasks.filter((task)=> task.completed===false )
+    }
+}
+console.log(tasks.getTasksToDo())
